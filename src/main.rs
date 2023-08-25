b3be28cc91983a6b463a13838e98ca13aa0cfd06
Valentine Briese <valentinegb@icloud.com>
2023-08-24 22:20:53 -0700

    Make the thing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use std::{env::args, io::Write, path::PathBuf, process::Command};
+
+use tempfile::NamedTempFile;
+
 fn main() {
-    println!("Hello, world!");
+    let path = PathBuf::from(args().nth(1).unwrap())
+        .canonicalize()
+        .expect("failed to canonicalize executable path");
+    let ovmf = include_bytes!("OVMF.fd");
+    let mut ovmf_file = NamedTempFile::new().expect("failed to create temporary ovmf file");
+
+    ovmf_file
+        .write_all(ovmf)
+        .expect("failed to write to temporary ovmf file");
+
+    Command::new(env!("CARGO_BIN_FILE_UEFI_RUN"))
+        .args([
+            "-b",
+            ovmf_file
+                .path()
+                .to_str()
+                .expect("failed to turn temporary ovmf file path into `&str`"),
+        ])
+        .arg(path)
+        .status()
+        .expect("failed to execute uefi-run");
 }
